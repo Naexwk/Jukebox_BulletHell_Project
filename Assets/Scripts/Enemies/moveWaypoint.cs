506e99813521966197d9dace04269fbc9dde8076
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-10-09 16:55:06 -0600

    minorStuff

---
+++ Assets/Scripts/Enemies/moveWaypoint.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveWaypoint : MonoBehaviour
+{
+    public float speed = 5f;
+    private bool change = true;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Update(){
+        if(change){
+            waypointRight();
+        }
+        else {
+            waypointLeft();
+        }
+    }
+    void waypointRight(){
+        transform.Translate(speed*Time.deltaTime,0,0);
+    }
+    void waypointLeft(){
+        transform.Translate(-speed*Time.deltaTime,0,0);
+    }
+     void OnTriggerStay2D(Collider2D otherCollider)
+    {
+        Debug.Log("HI");
+        change = !change;
+    }
+}
